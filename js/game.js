2b8f5f1d855fb7f2048d0135fb9c38facf407c3e
7nahoge <replica.sub@gmail.com>
2026-07-08 06:09:11 +0900

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -488,8 +488,8 @@ document.addEventListener("keydown",e=>{
 // touch
 let tx,ty;
 const SWIPE = 30;
-const TOUCH_MOVE_INTERVAL = 250;
-const TOUCH_DROP_INTERVAL = 250;
+const TOUCH_MOVE_INTERVAL = 300;
+const TOUCH_DROP_INTERVAL = 300;
 let lastTouchMove = 0;
 let lastTouchDrop = 0;
 
